24911574fdf3cf90b38a0a85c29f0f2b10169a03
Mandhoj Thing <theengmandhoj@gmail.com>
2025-03-02 21:34:14 +0545

    Successfully loaded index.html in django

--- Backend/myenv/Scripts/ai_blog_app/ai_blog_app/settings.py
+++ Backend/myenv/Scripts/ai_blog_app/ai_blog_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog_generator',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,8 @@ ROOT_URLCONF = 'ai_blog_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        #defining the path for the html template file
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
